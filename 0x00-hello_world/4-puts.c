d655bab15e342b22aff5d301f0dd122f6ea57795
Emmastro <emurairi18@alastudents.org>
2020-09-16 20:57:59 +0200

    Follow betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+/**
+ * main : main block
+ * Return: 0
+ */
 int main(){
-puts("Programming is like building a multilingual puzzle");
-return 0;
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
