cdfc5c3d5deaea377e82515064b0e061f2f97999
Emmastro <emurairi18@alastudents.org>
2020-09-16 21:42:42 +0200

    Fix permission levels

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc -E $CFILE >> c
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
