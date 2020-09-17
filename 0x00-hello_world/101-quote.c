1a8616a1f98d7c1015c6d5a48a58fb5cef4530dc
Emmastro <emurairi18@alastudents.org>
2020-09-17 23:35:31 +0200

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
+}
