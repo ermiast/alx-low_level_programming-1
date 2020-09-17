cf393b433c310dd567877d87991aef24912de69c
Emmastro <emurairi18@alastudents.org>
2020-09-17 16:05:50 +0200

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	for (char i = 0; i < 26; i++)
+	{
+		putc(i+97, stdout);
+	}
+	
+	return (0);
+}
