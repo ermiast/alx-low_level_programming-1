f84192bc4fc7e1d7a32c79982826003f6dd49f52
Emmastro <emurairi18@alastudents.org>
2020-09-22 18:51:49 +0200

    0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 
-	char c[10] = "Holberton";
+	char c[10] = "Holberton\n";
 
 	for (int i = 0; c[i] != '\0'; i++)
 	{
 		_putchar(c[i]);
 	}
-	_putchar('\n');
+	/*_putchar('\n');*/
 
 	return (0);
 }
