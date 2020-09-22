6bc84a8f84c9bd36c4a38f82e74aac2fa0ec80d8
Emmastro <emurairi18@alastudents.org>
2020-09-22 19:21:35 +0200

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,16 @@
 
 void print_alphabet_x10(void)
 {
-	char c = 'a';
+	char c;
 
 	for (int i = 0; i < 10; i++)
 	{
+		c = 'a';
 		while (c <= 'z')
 		{
 			_putchar(c);
 			c++;
 		}
+		_putchar('\n');
 	}
-	
 }
