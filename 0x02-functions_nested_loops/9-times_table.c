e51cf44230abe2bada627defe4c864962003da44
Emmastro <emurairi18@alastudents.org>
2020-09-22 22:05:09 +0200

    Submit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints times table
+ * Return:void
+ */
+
+void times_table(void)
+{
+
+int a = 0;
+int b;
+int rep;
+while (a <= 9)
+{
+	b = 0;
+	while (b <= 9)
+	{
+		rep = a * b;
+		if (rep < 10)
+		{
+			_putchar('0' + rep);
+		}
+		else
+		{
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+
+		if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		b++;
+	}
+	_putchar('\n');
+	a++;
+}
+
+
+}
