8b89a3b4e0744ff147d179f8a44d84547681d464
Emmastro <emurairi18@alastudents.org>
2020-09-23 19:07:41 +0200

    Submit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,41 +11,41 @@ void print_times_table(int n)
 
 int a = 0, rep;
 
-if (n >= 0 && n <= 15)
+if (n <= 0 || n >= 15)
+	return;
+
+while (a <= n)
 {
-	while (a <= n)
+	for (int b = 0; b <= n; b++)
 	{
-		for (int b = 0; b <= n; b++)
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
 		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else if (rep < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
 		}
-		_putchar('\n');
-		a++;
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
+	_putchar('\n');
+	a++;
 }
 }
