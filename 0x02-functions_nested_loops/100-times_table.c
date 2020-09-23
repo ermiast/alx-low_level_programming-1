b51b8ba55956f9094faa025631d40ce20c72827f
Emmastro <emurairi18@alastudents.org>
2020-09-23 18:48:05 +0200

    Submit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,42 +6,42 @@
  * Return:void
  */
 
-void print_times_table(n)
+void print_times_table(int n)
 {
 
 int a = 0;
-int b;
 int rep;
 
 if (n >= 0 && n <= 15)
 {
 	while (a <= n)
 	{
-		b = 0;
-		while (b <= n)
+		for (int b = 0; b <= n; b++)
 		{
 			rep = a * b;
 			if (b == 0)
-			{
 				_putchar('0' + rep);
-			}
 			else if (rep < 10)
 			{
 				_putchar(' ');
 				_putchar('0' + rep);
 			}
-			else
+			else if (rep < 100)
 			{
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
 			}
-
-			if (b < 9)
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			b++;
 		}
 		_putchar('\n');
 		a++;
