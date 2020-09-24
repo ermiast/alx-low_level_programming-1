98888948738c9a70de0c035f0d47f452122b8dc9
Emmastro <emurairi18@alastudents.org>
2020-09-24 22:13:50 +0200

    Submit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,23 +9,22 @@
 void print_triangle(int size)
 {
 
-	int i = 0, ii;
+	int i = 1, ii;
 
-	while (i < size && size > 0)
+	while (i <= size && size > 0)
 	{
 		ii = 0;
-		while (ii < i)
+		while (ii < size - i)
 		{
 			_putchar(' ');
 			ii++;
 		}
-        ii = 0;
-		while (ii < size - i)
+		ii = 0;
+		while (ii < i)
 		{
 			_putchar('#');
 			ii++;
 		}
-
 
 		_putchar('\n');
 		i++;
