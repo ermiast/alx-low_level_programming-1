73910833104068bf35d4f81710e3cb168af1ba71
Emmastro <emurairi18@alastudents.org>
2020-09-24 13:38:56 +0200

    Submit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - prints most numbers
+ * print_most_numbers - prints numbers
  * Return:0
  */
 
 void print_most_numbers(void)
 {
-	for (char n = '0'; n <= '9'; n++)
+	char n = 0;
+
+	while (n <= 9)
 	{
-		if (n != '2' && n != '4')
+		if (n != 2 && n != 4)
 		{
-			_putchar(n);
+			_putchar('0' + n);
 		}
 
+		n++;
 	}
 	_putchar('\n');
 
