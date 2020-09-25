2dab87ddbd0133c1c5aeeb76584786ee999540dd
Emmastro <emurairi18@alastudents.org>
2020-09-25 18:49:23 +0200

    Submit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,15 +8,17 @@
 int main(void)
 {
 
-unsigned int long n = 612852475143;
+unsigned int long n = 612852475143, a = 2;
 
-for (unsigned int long a = 2; a < n; a++)
+while (a < n)
 {
 
 	if (n % a == 0)
 	{
 		n = n / a;
 	}
+	a++;
+
 }
 
 printf("%lu \n", n);
