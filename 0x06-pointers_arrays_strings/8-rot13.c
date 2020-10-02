d49aec2f76d36c81a0ff4e377a3b3e62ea5e7bb2
Emmastro <emurairi18@alastudents.org>
2020-10-02 19:25:46 +0200

    Fix 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -17,13 +17,11 @@ for (i = 0; s[i] != '\0'; i++)
 {
 	for (ii = 0; ii < 54; ii++)
 	{
-		if ((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
+		if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
+		&& s[i] == input[ii])
 		{
-			if (s[i] == input[ii])
-			{
-				s[i] = output[ii];
-				break;
-			}
+			s[i] = output[ii];
+			break;
 		}
 	}
 }
