e8db6d0d993feebb9a1ef5924ac7dca587a01988
Emmastro <emurairi18@alastudents.org>
2020-10-05 21:54:30 +0200

    Submit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,26 +1,24 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * *print_chessboard - description
+ * @a: board
+ * @needle: pointer
+ * Return: pointer
  */
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *p;
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    p = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", p);
-    p = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", p);
-    return (0);
+
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+    for (j = 0; j < 8; j++)
+    {
+        _putchar(a[i][j]);   
+    }
+    _putchar('\n');
+}
+
 }
