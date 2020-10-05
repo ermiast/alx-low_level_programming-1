4d1b00e3f7a51adafb6aae8525a4e1cb5447a011
Emmastro <emurairi18@alastudents.org>
2020-10-05 17:10:41 +0200

    Submit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,14 +11,9 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
 
-while (n > 0)
-{
+for (unsigned int i = 0; n > 0 ; i++, n--)
 	s[i] = b;
-	i++;
-	n--;
-}
 
 return (s);
 }
