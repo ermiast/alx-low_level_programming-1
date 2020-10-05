d7f4cab3b71844572a16931d62da1de4373a9da9
Emmastro <emurairi18@alastudents.org>
2020-10-05 21:16:02 +0200

    Submit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,21 +10,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; haystack[i] >= '\0'; i++)
+for (i = 0; haystack[i] > '\0'; i++)
+{
+	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
 	{
-		for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+		if (haystack[j] != needle[j - i])
 		{
-			if (haystack[j] != needle[j - i])
-			{
-				break;
-			}
-		}
-		if (needle[j - i] == '\0')
-		{
-			return (haystack + i);
+			break;
 		}
 	}
+	if (needle[j - i] == '\0')
+	{
+		return (haystack + i);
+	}
+}
 return (0);
 }
