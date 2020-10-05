a1f58a4757caec0e2e35c342f4929295374faae9
Emmastro <emurairi18@alastudents.org>
2020-10-05 20:10:33 +0200

    Submit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,32 @@
 #include "holberton.h"
 
 /**
- * *_strchr - fills memory with a constant byte.
- * @s: pointer to put the constant
- * @c: constant
- * Return: s
+ * *_strspn - description
+ * @s: string
+ * @accept: pointer
+ * Return: int
  */
 
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int i, j, cmpt = 0;
 
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] > '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				cmpt++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			break;
+		}
+		
+	}
+	return (cmpt);
 }
