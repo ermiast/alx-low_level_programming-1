0bfc9ce9a671791c603eb9258f6a9ed992f28fe5
Emmastro <emurairi18@alastudents.org>
2020-10-05 10:44:49 +0200

    initialize project 0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i <= n ; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+# Learning pointers, arrays and strings
+
+What are pointers to pointers and how to use them
+What are multidimensional arrays and how to use them
+What are the most common C standard library functions to manipulate strings
