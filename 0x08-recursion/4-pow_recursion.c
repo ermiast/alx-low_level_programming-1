b13d389317d15ff958667e919e36e55a01b80b04
Emmastro <emurairi18@alastudents.org>
2020-10-07 22:16:40 +0200

    Submit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,16 +12,11 @@ int _pow_recursion(int x, int y)
 {
 
 if (y < 0)
-{
 	return (-1);
-}
 else if (y == 1)
-{
 	return (x);
-}
-else if (y == 0){
+else if (y == 0)
 	return (1);
-}
 
 return (x * _pow_recursion(x, y - 1));
 
