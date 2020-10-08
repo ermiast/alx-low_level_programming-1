d94b08c29ed7186d3155ac4c578861e0b95eb3ec
Emmastro <emurairi18@alastudents.org>
2020-10-08 09:30:59 +0200

    Solve prime number

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -6,5 +6,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int square(int n, int val);
 int _sqrt_recursion(int n);
+int check_prime(int n, int resp);
 int is_prime_number(int n);
 int is_palindrome(char *s);
