b4af62604c4c826e0c02319419119d32a72a027f
Emmastro <emurairi18@alastudents.org>
2020-10-08 10:11:29 +0200

    Submit

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -5,7 +5,22 @@
  * Return: 0 or 1
  */
 
-int last_index(char *);
+/**
+ * last_index - returns the last index of a string (counts the null char)
+ * @s: pointer the string
+ * Return: int
+ */
+
+int last_index(char *s)
+{
+int n = 0;
+
+if (*s > '\0')
+	n += last_index(s + 1) + 1;
+
+return (n);
+}
+
 
 int is_palindrome(char *s)
 {
@@ -29,20 +44,4 @@ else if (s[start] != s[end])
 	return (0);
 else
 	return (check(s, start + 1, end - 1));
-}
-
-/**
- * last_index - returns the last index of a string (counts the null char)
- * @s: pointer the string
- * Return: int
- */
-
-int last_index(char *s)
-{
-int n = 0;
-
-if (*s > '\0')
-	n += last_index(s + 1) + 1;
-
-return (n);
 }
