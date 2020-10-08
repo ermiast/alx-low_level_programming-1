5b66a12498078fae30672c2dce06843d54ca37da
Emmastro <emurairi18@alastudents.org>
2020-10-08 10:12:55 +0200

    Submit palindrome

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -8,5 +8,6 @@ int square(int n, int val);
 int _sqrt_recursion(int n);
 int check_prime(int n, int resp);
 int is_prime_number(int n);
+int last_index(char *s);
 int check(char *s, int start, int end);
 int is_palindrome(char *s);
