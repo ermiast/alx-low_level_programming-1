3f22884bb3b4a7a084763ed44f80c52f252f2605
Emmastro <emurairi18@alastudents.org>
2020-10-12 19:20:14 +0200

    Submit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,11 +10,6 @@
 int main(int argc, char const *argv[])
 {
 int i = 0;
-if (argc != 3)
-{
-	printf("Error\n");
-	return (1);
-}
 
 while (argc--)
 {
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,6 +10,12 @@
 int main(int argc, char const *argv[])
 {
 (void)argc;
+
+if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
 printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
