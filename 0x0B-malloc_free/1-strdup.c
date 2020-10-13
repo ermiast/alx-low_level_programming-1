593f0dc780b9861e7c5709f23d440bbd5bbd9583
Emmastro <emurairi18@alastudents.org>
2020-10-13 17:55:07 +0200

    Submit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,19 +14,23 @@ char *_strdup(char *str)
 int i = 0, size = 0;
 char *m;
 
+if (str == NULL)
+	return (NULL);
+
 for (; str[size] != '\0'; size++)
 ;
 
 /*+1 on the size puts the end of string character*/
 m = malloc(size * sizeof(*str) + 1);
 
-if (size > 0 && m != 0)
+if (m == 0)
+	return (NULL);
+else
 {
 	for (; i < size; i++)
 		m[i] = str[i];
 	m[i] = '\0';
-	return (m);
 }
-free(m);
-return (NULL);
+
+return (m);
 }
