1b14ade70eeffc1c88aa78d999481702cda411ff
Emmastro <emurairi18@alastudents.org>
2020-10-14 11:43:24 +0200

    Submit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,13 +26,17 @@ return (size);
 
 char *str_concat(char *s1, char *s2)
 {
+int size1, size2, i;
+char *m;
+
 if (s1 == NULL)
 	s1 = "\0";
 if (s2 == NULL)
 	s2 = "\0";
 
-int size1 = _strlen(s1), size2 = _strlen(s2), i;
-char *m = malloc((size1 + size2) * sizeof(char) + 1);
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+m = malloc((size1 + size2) * sizeof(char) + 1);
 
 for (i = 0; i <= size1 + size2; i++)
 {
