c01bc7a11352c3d0e15266ce487d9e614701f089
Emmastro <emurairi18@alastudents.org>
2020-10-15 22:16:28 +0200

    Submit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,14 +26,17 @@ return (size);
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i, j;
-char *m = malloc(_strlen(s1) + n * sizeof(char) + 1);
+char *m;
 
-if (m == 0)
-	return (NULL);
 if (s1 == NULL)
 	s1 = "";
 if (s2 == NULL)
 	s2 = "";
+
+m = malloc(_strlen(s1) + n * sizeof(char) + 1);
+
+if (m == 0)
+	return (NULL);
 
 for (i = 0; s1[i] != '\0'; i++)
 	m[i] = s1[i];
