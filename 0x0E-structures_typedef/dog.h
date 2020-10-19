b8c669f167de31c758441e592611254e2b4111b2
Emmastro <emurairi18@alastudents.org>
2020-10-19 21:20:19 +0200

    Submit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,9 +13,10 @@ typedef struct dog
 	char *name;
 	float age;
 	char * owner;
-};
+} dog_struct;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-#endif /*DOG_H*/
+dog_struct *new_dog(char *name, float age, char *owner);
+void free_dog(dog_struct *d);
+#endif
