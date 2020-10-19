432c1a35f0d7518493a083d2cebc883f69d6d764
Emmastro <emurairi18@alastudents.org>
2020-10-19 22:44:29 +0200

    Submit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,29 +15,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 struct dog *mydog;
-char *n, *o;
-
 mydog = malloc(sizeof(struct dog));
 
 if (mydog == NULL)
 	return (NULL);
 
-n = malloc(sizeof(name));
-if (n == NULL)
-	return (NULL);
-
-o = malloc(sizeof(owner));
-if (o == NULL)
-	return (NULL);
-
-n = strdup(name);
-o = strdup(owner);
-
-mydog->name = n;
-//mydog->name* = strdup(name);
+strcpy(mydog->name, name);
 mydog->age = age;
-mydog->owner = o;
-//mydog->owner = strdup(owner);
+strcpy(mydog->owner, owner);
 
 return (mydog);
 }
