483fd5b01d5e509e2daaab363805e5ba42cd3201
Emmastro <emurairi18@alastudents.org>
2020-10-21 22:04:23 +0200

    Submit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,15 +1,41 @@
-#include "dog.h"
+#include "3-calc.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * print_name - prints a name
- * @d: struct dog
- * @name: string for name
- * @age: integer for age
- * @owner: string for owners
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: terms and operators
+ * Return: 0
  */
 
-void print_name(char *name, void (*f)(char *))
+int main(int argc, char *argv[])
 {
+	int a, b;
+	int (*calculate)(int, int);
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	calculate = get_op_func(argv[2]);
+	if (calculate == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", calculate(a, b));
+
+	return (0);
 }
