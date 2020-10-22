c87586761f61b25ef3f4b5fe551b3147e6a24754
Emmastro <emurairi18@alastudents.org>
2020-10-22 11:32:32 +0200

    Submit

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * op_add - addition operator
  * @a: first int
@@ -42,11 +39,6 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
@@ -58,10 +50,5 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
