58d33b89ecc4afa21081783afc5ac9734bc17eac
Emmastro <emurairi18@alastudents.org>
2020-10-22 10:41:06 +0200

    Submit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,37 @@
 #include "3-calc.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
-  * main - prints the result between two operators.
-  * @argc: number of arguments received.
-  * @argv: array of arguments received.
-  *
-  * Return: 0 (success).
-  */
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: terms and operators
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
-	int num1 = 0;
-	int num2 = 0;
-	int res;
+	int (*calculate)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	res = get_op_func(argv[2])(num1, num2);
+	calculate = get_op_func(argv[2]);
+	if (calculate == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	printf("%d\n", res);
+	printf("%d\n", calculate(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
