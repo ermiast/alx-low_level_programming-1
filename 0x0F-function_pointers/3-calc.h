105473f00da32820c430b501e14d544d3edbdc7d
Emmastro <emurairi18@alastudents.org>
2020-10-22 08:43:32 +0200

    Submit

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -13,10 +13,10 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 #endif
