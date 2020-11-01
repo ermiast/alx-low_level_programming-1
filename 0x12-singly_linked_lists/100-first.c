11a696da60ea8f7d9b6bdca4f2f15cc53fde3842
Emmastro <emurairi18@alastudents.org>
2020-11-01 19:32:10 +0100

    Submit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,5 +2,7 @@
 
 void __attribute__ ((constructor)) premain()
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+);
 }
