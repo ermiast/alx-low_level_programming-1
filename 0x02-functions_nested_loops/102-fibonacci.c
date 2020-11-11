eb59d00e8a6689325643910401ba503ce4ff5cef
Emmastro <emurairi18@alastudents.org>
2020-11-11 11:43:08 +0100

    Submit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 	int i = 0;
 	long int a = 0, b = 1, next;
 
-	while (i <= 50)
+	while (i < 50)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 50)
+		if (i < 49)
 		{
 			printf(", ");
 		}
