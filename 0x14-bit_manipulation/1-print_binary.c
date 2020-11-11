2ea48f12b9935a07e80fa6399bd820138e6e03dd
Emmastro <emurairi18@alastudents.org>
2020-11-11 10:49:08 +0100

    Submit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,13 @@
 #include "holberton.h"
 
 /**
- * print_binary - prints decimal to binary
- * Description: first, finds the biggest exponent for 2, then go down
- * to find the smaller values
- * @n: decimal number
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed in binary
  */
-
 void print_binary(unsigned long int n)
 {
-int i, k, andmask, change;
-int firstOne = 0;
-unsigned long int max = 1;
+	if (n > 1)
+		print_binary(n >> 1);
 
-for (max = 0, change = 0; max <=n; max = max << 1, change++)
-
-for (i = change; i >= 0 ; i--)
-{
-andmask = 1 << i;
-k = n & andmask;
-
-if (k != 0 && firstOne == 0)
-	firstOne = 1;
-
-if (firstOne != 0)
-	k == 0 ? _putchar('0') : _putchar('1');
-}
-if (firstOne == 0)
-	_putchar('0');
+	_putchar((n & 1) + '0');
 }
