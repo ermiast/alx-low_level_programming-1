fb33fc808a7caca3e8b6600930c319062c6393ba
Emmastro <emurairi18@alastudents.org>
2020-11-11 09:16:27 +0100

    Submit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,12 +19,15 @@ int main(int ac, char *av[])
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (ac != 3)
 		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
+
 	input_fd = open(av[1], O_RDONLY);
 	if (input_fd == -1)
 		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
+
 	output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (output_fd == -1)
 		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+
 	do {
 		istatus = read(input_fd, buf, MAXSIZE);
 		if (istatus == -1)
@@ -42,11 +45,13 @@ int main(int ac, char *av[])
 			}
 		}
 	} while (istatus > 0);
+
 	istatus = close(input_fd);
 	if (istatus == -1)
 		dprintf(SE, "Error: Can't close fd %d\n", input_fd), exit(100);
 	ostatus = close(output_fd);
 	if (ostatus == -1)
 		dprintf(SE, "Error: Can't close fd %d\n", output_fd), exit(100);
+
 	return (0);
 }
