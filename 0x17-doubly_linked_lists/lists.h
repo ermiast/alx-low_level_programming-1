7f97ce8b278339cca9a4f770bf2dd20fb92d9779
Emmastro <emurairi18@alastudents.org>
2020-12-13 21:39:36 +0100

    Submit

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -22,6 +22,8 @@ typedef struct dlistint_s
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+void free_dlistint(dlistint_t *head);
 
 #endif
