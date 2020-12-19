3cac61bad3ee22550db5b2a0e2e1a9e1d8987825
Emmastro <emurairi18@alastudents.org>
2020-12-19 15:01:46 +0100

    Submit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -37,7 +37,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->next = current;
 	new->prev = current->prev;
-	current->prev->next = new;
+	if (current->prev != NULL)
+		current->prev->next = new;
 
 	/*TODO: Handle special case when idx is 0 and last index*/
 
