c5afe0e5d87d6680e8d5049972f0840c48362092
Emmastro <emurairi18@alastudents.org>
2021-01-24 18:40:05 +0100

    Submit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,12 +14,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 hash_node_t *node;
 unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
-if (ht == NULL || key == '\n' || value == '\n')
-	return (NULL);
+if (ht == NULL || *key == '\n' || *value == '\n')
+	return (0);
 
 node = ht->array[index];
 
-/*Handle collision by chaining*/
 while (node != NULL)
 	node = node->next;
 
